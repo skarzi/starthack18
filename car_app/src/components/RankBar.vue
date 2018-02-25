6f75058b219ddd5a892d24f78bbb09d21d3aee92
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2018-02-25 03:36:38 +0100

    add rank bar draft

---
+++ car_app/src/components/RankBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bar-container">
+    <div class="bar-label" :class="{'color': labelColor}">
+    </div>
+    <div class="bar-body">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      labelColor: 'orange'
+    }
+  }
+}
+</script>
+<style>
+</style>
--- car_app/src/pages/Main.vue
+++ car_app/src/pages/Main.vue
@@ -4,6 +4,14 @@
     <div class="google-search-input-container">
       <GoogleSearchInput class="google-search-input"/>
     </div>
+    <div class="bottom-components">
+      <p :style="'color: ' +  color + ';'">{{rank / 10}}</p>
+      <v-progress-linear v-model="rank" height="20" :color="color"
+                         buffer-value="50"
+      ></v-progress-linear>
+      <div class="info-window">
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -14,6 +22,17 @@ export default {
   components: {
     GoogleMap: GoogleMap,
     GoogleSearchInput: GoogleSearchInput
+  },
+  data () {
+    return {
+      rank: 1.2,
+      color: 'primary'
+    }
+  },
+  methods: {
+    updateRank (points) {
+      this.rank += points
+    }
   }
 }
 </script>
@@ -34,5 +53,12 @@ export default {
   .google-search-input {
     width: 85%;
     left: 20px;
+  }
+  .bottom-components {
+    position: absolute;
+    bottom: 75px;
+    right: 65%;
+    left: 50px;
+    z-index: 100;
   }
 </style>
